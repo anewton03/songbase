f21d6ec6ea269e8b29b2166aff28d6a9049d0484
alexis newton <anewton@udel.edu>
2017-11-09 10:48:23 -0500

    added templates

--- songbase.py
+++ songbase.py
@@ -1,14 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>hello world!</h1>'
+    #return '<h1>hello world!</h1>'
+    return render_template('index.html')
 
 
 @app.route('/user/<string:name>/')
 def get_user(name):
-    return 'hello %s your age is %d' % (name, 21)
+    #return 'hello %s your age is %d' % (name, 21)
+    return render_template('user.html', user_name=name)
+
+
+@app.route('/my-song')
+def get_all_songs():
+    songs = [
+            'Song1',
+            'Song2',
+            'Song3'
+            ]
+    return render_template('my-song.html', songs=songs)
 
 
 @app.route('/users')
@@ -17,4 +29,4 @@ def show_all_users():
 
 
 if __name__ == '__main__':
-    app.run(port=2000)
+    app.run()
