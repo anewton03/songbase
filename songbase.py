9d54989e9f2a57431d26573ed854eb8ead79819d
alexis newton <anewton@udel.edu>
2017-11-14 10:44:47 -0500

    added form demo

--- songbase.py
+++ songbase.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '18902193'
 
 @app.route('/')
 def index():
     #return '<h1>hello world!</h1>'
     return render_template('index.html')
+
+
+@app.route('/form-demo', methods=['GET', 'POST'])
+def form_demo():
+    # how to get form data is different for GET vs. POST
+    if request.method == 'GET':
+        first_name = request.args.get('first_name')
+        if first_name:
+            return render_template('form-demo.html', first_name=first_name)
+        else:
+            return render_template('form-demo.html', first_name=session.get('first_name'))
+    if request.method == 'POST':
+        session['first_name'] = request.form['first_name']
+        # return render_template('form-demo.html', first_name=first_name)
+        return redirect(url_for('form_demo'))
 
 
 @app.route('/user/<string:name>/')
@@ -29,4 +45,4 @@ def show_all_users():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
